becb85d0c316babf47fd997758a51bda38b15418
Jayammo <josgood2gmail.com>
2021-01-12 19:02:11 -0500

    update code formatting

--- src/component/Pages/Home/Home.jsx
+++ src/component/Pages/Home/Home.jsx
@@ -10,8 +10,7 @@ import {
   CardContent,
   CardMedia,
 } from "@material-ui/core";
-import cross from './../../../assets/cross.jpg';
-
+import cross from "./../../../assets/cross.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(2),
   },
   media: {
-    maxHeight: '400px',
+    maxHeight: "400px",
   },
 }));
 
@@ -28,33 +27,34 @@ const Home = (props) => {
 
   return (
     <Grid container>
-      <Grid container justify="center" alignItems="center" item md={12} >
+      <Grid container justify="center" alignItems="center" item md={12}>
         <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image={cross}
-            title="Content"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Mission Statement
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              A one-sentence statement describing the reason an organization or 
-            program exists and used to help guide decisions about priorities, actions, and responsibilities.  
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image={cross}
+              title="Content"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Mission Statement
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A one-sentence statement describing the reason an organization
+                or program exists and used to help guide decisions about
+                priorities, actions, and responsibilities.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
       </Grid>
     </Grid>
   );
